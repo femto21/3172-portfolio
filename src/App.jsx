1dcb76c9fc2373a35cce15f4d5fde5832c5739aa
Arpit Gahlot <arpitgahlot2003@gmail.com>
2025-11-16 19:02:30 -0400

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Error from "./pages/Error";
 
 function App() {
   return (
